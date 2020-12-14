fc26146df95a794b6f48916f50a8470b5e2a7502
bramwell simiyu <simiyub.engineer@gmail.com>
2020-12-14 10:22:45 +0000

    packages and modules

---
+++ learn/complete-python-bootcamp-worksheets/section9ModulesandPackages/section9ModulesandPackages.py
@@ -0,0 +1,10 @@
+from moduleToImport import my_func
+from utils.util import my_func as utils_func
+from utils.sub_package.sub_util import my_func as sub_func
+from utils.sub_package.sub_util import my_func as sub_func
+from check_if_run_directly import my_func as check_func
+my_func()
+utils_func()
+sub_func()
+check_func()
+
---
+++ learn/complete-python-bootcamp-worksheets/section9ModulesandPackages/utils/util.py
@@ -0,0 +1,2 @@
+def my_func():
+    print("I am my_func in util")
