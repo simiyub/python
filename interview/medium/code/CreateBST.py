0f441594b5e4b3424cdc9d13d9f8a4faccd5aafb
bramwell simiyu <simiyub.engineer@gmail.com>
2022-01-31 16:00:28 +0000

    moving workstations

--- interview/medium/code/CreateBST.py
+++ interview/medium/code/CreateBST.py
@@ -46,11 +46,41 @@ class CreateBST:
     def remove(self, value, parent):
         current = self
         while current is not None:
-            if value < self.value:
+            if value < current.value:
                 parent = current
                 current = current.left
-            elif value > self.value:
+            elif value > current.value:
                 parent = current
                 current = current.right
             else:
-                if current.l
+                if current.left is not None and current.right is not None:
+                    current.value = current.right.get_minimum_value()
+                    current.right.remove(current.value, current)
+
+                #root node should not be removed
+                elif parent is None:
+                    if current.left is not None:
+                        current.value = current.left.value
+                        current.right = current.left.right
+                        current.left = current.left.left
+
+                    elif current.right is not None:
+                        current.value = current.right.get_minimum_value
+                        current.left = current.right.left
+                        current.right = current.right.right
+                    else:
+                        current.value = None
+
+                elif parent.left == current:
+                    parent.left = current.left if current.left is not None else current.right
+                elif parent.right == current:
+                    parent.right == current.left if current.left is not  None else current.right
+                break
+        return self
+
+def get_minimum_value(self):
+    current = self
+    while current.left is not None:
+        current = current.left
+    return current.value
+
