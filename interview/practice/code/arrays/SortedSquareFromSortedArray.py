57bdf1dc66d373b0bb55d349220f24f078908e16
bramwell simiyu <simiyub.engineer@gmail.com>
2022-03-07 17:21:48 +0000

    fixed edge case

--- interview/practice/code/arrays/SortedSquareFromSortedArray.py
+++ interview/practice/code/arrays/SortedSquareFromSortedArray.py
@@ -4,4 +4,18 @@ Given an array of sorted integers, this function returns a sorted array of the s
 
 
 def sorted_square_array(array):
-    return [value **2 for value in array]
+    result = [0 for _ in array]
+    smaller_value_index = 0
+    larger_value_index = len(array) - 1
+
+    for index in reversed(range(len(array))):
+        smaller_value = array[smaller_value_index]
+        larger_value = array[larger_value_index]
+
+        if (abs(smaller_value) > abs(larger_value)):
+            result[index] = smaller_value ** 2
+            smaller_value_index += 1
+        else:
+            result[index] = larger_value ** 2
+            larger_value_index += 1
+    return result
--- interview/practice/tests/arrays/TestSortedSquareArrayFromSortedArray.py
+++ interview/practice/tests/arrays/TestSortedSquareArrayFromSortedArray.py
@@ -1,11 +1,14 @@
 import unittest
 from interview.practice.code.arrays.SortedSquareFromSortedArray import sorted_square_array
 
-class SortedSquareArrayFromSortedArray(unittest.TestCase):
-    def test_sorted_square_array(self):
+class TestSortedSquareArrayFromSortedArray(unittest.TestCase):
+    def test_sorted_square_array1(self):
         self.assertEqual([1, 4, 9], sorted_square_array([1, 2, 3]) )
-    def test_sorted_square_array(self):
+    def test_sorted_square_array2(self):
         self.assertEqual([9, 25, 81], sorted_square_array([3, 5, 9]) )
+
+    def test_sorted_square_array3(self):
+        self.assertEqual([1, 4], sorted_square_array([-2, -1]) )
 
 if __name__ == '__main__':
     unittest.main()
